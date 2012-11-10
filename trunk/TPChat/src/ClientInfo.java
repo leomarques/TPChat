bab6d1c6b44481f92a783cd4dcf61d7e5ae6ac85
leommarques7@gmail.com <leommarques7@gmail.com@839845dc-66d0-19fd-e816-7743a85681c2>
2012-11-10 18:57:38 +0000

    server-side pronto

---
+++ trunk/TPChat/src/ClientInfo.java
@@ -0,0 +1,11 @@
+/**
+ * ClientInfo class contains information about a client, connected to the server.
+ */
+
+import java.net.Socket;
+
+public class ClientInfo {
+	public Socket mSocket = null;
+	public ClientListener mClientListener = null;
+	public ClientSender mClientSender = null;
+}
