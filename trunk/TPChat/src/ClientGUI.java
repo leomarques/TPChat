f235d480e724d0e9aba2d2d2e43003878d9e408f
leommarques7@gmail.com <leommarques7@gmail.com@839845dc-66d0-19fd-e816-7743a85681c2>
2012-11-11 01:35:51 +0000

    +gui textarea mostra as mensagens mas a entrada ainda é pelo console

---
+++ trunk/TPChat/src/ClientGUI.java
@@ -0,0 +1,48 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ClientGUI extends JFrame {
+	public static final String SERVER_HOSTNAME = "localhost";
+	public static final int SERVER_PORT = 2002;
+
+	private Panel panel;
+
+	public ClientGUI() {
+		setTitle("TPChat");
+		setSize(640, 480);
+		setLocationRelativeTo(null);
+
+		panel = new Panel();
+		add(panel);
+
+		setVisible(true);
+	}
+
+	public JTextArea getTextArea() {
+		return panel.textArea;
+	}
+
+	public JTextField getTextField() {
+		return panel.textField;
+	}
+}
+
+class Panel extends JPanel {
+	public JTextArea textArea;
+	public JTextField textField;
+
+	Panel() {
+		textArea = new JTextArea();
+		textField = new JTextField();
+
+		textArea.setEditable(false);
+
+		setLayout(new BorderLayout());
+		add(textArea, BorderLayout.PAGE_START);
+		add(textField, BorderLayout.PAGE_END);
+	}
+}
---
+++ trunk/TPChat/src/TPChatClient.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class TPChatClient {
+	public static final String SERVER_HOSTNAME = "localhost";
+	public static final int SERVER_PORT = 2002;
+
+	public static void main(String[] args) {
+		ClientGUI clientGUI = new ClientGUI();
+		JTextArea textArea = clientGUI.getTextArea();
+		JTextField textField = clientGUI.getTextField();
+		
+		textArea.append("Welcome to TPChat!\n");
+
+		BufferedReader in = null;
+		PrintWriter out = null;
+		try {
+			// Connect to Server
+			Socket socket = new Socket(SERVER_HOSTNAME, SERVER_PORT);
+			in = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			out = new PrintWriter(new OutputStreamWriter(
+					socket.getOutputStream()));
+			textArea.append("Connected to server " + SERVER_HOSTNAME + ":"
+					+ SERVER_PORT);
+		} catch (IOException ioe) {
+			textArea.append("Can not establish connection to "
+					+ SERVER_HOSTNAME + ":" + SERVER_PORT);
+			ioe.printStackTrace();
+			System.exit(-1);
+		}
+		
+		textArea.append("\n\n");
+		
+		Receiver receiver = new Receiver(textArea, in);
+		receiver.start();
+		
+		Sender sender = new Sender(out);
+		sender.setDaemon(true);
+		sender.start();
+	}
+
+}
